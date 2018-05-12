fa302aff04a1344ef8fc0a079977d722b52655e1
Jose Sebio <jalopezsebio@gmail.com>
2018-05-12 22:26:57 +1000

    Update to elixir 1.6

--- mix.exs
+++ mix.exs
@@ -5,9 +5,9 @@ defmodule Percebex.Mixfile do
     [
       app: :percebex,
       version: "0.1.0",
-      elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
-      elixirc_paths: elixirc_paths(Mix.env),
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps()
     ]
   end
